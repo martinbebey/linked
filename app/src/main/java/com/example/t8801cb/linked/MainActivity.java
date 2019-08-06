48460638704b915bd2ef540293e02f0242811317
Cyrille Martin Bebey-Tongo <Cyrille.BebeyTongo@external.fcagroup.com>
2019-08-06 19:35:23 -0400

    added login intent

--- app/src/main/java/com/example/t8801cb/linked/MainActivity.java
+++ app/src/main/java/com/example/t8801cb/linked/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.t8801cb.linked;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.t8801cb.linked.ui.login.LoginActivity;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the SignIn button */
+    public void signIn(View view)
+    {
+        Intent intent = new Intent(this, LoginActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = "shit";
+//        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
