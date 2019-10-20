e12a54c7282dd772ed91463df3bac332ceb522ae
Cyrille Martin Bebey-Tongo <Cyrille.BebeyTongo@external.fcagroup.com>
2019-10-20 12:04:32 -0400

    removed login activity

--- app/src/main/java/com/example/t8801cb/linked/MainActivity.java
+++ app/src/main/java/com/example/t8801cb/linked/MainActivity.java
@@ -6,9 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-
-import com.example.t8801cb.linked.ui.login.LoginActivity;
-
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,10 +20,10 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user taps the SignIn button */
     public void signIn(View view)
     {
-        Intent intent = new Intent(this, LoginActivity.class);
-//        EditText editText = (EditText) findViewById(R.id.editText);
-//        String message = "shit";
-//        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+//        Intent intent = new Intent(this, LoginActivity.class);
+////        EditText editText = (EditText) findViewById(R.id.editText);
+////        String message = "shit";
+////        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
     }
 }
