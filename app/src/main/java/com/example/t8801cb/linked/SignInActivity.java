a2e17505ae64df555a5beeeeb326fd4c71e8fcd3
Cyrille Martin Bebey-Tongo <Cyrille.BebeyTongo@external.fcagroup.com>
2019-11-18 21:23:00 -0500

    added empty home activity

--- app/src/main/java/com/example/t8801cb/linked/SignInActivity.java
+++ app/src/main/java/com/example/t8801cb/linked/SignInActivity.java
@@ -2,13 +2,23 @@ package com.example.t8801cb.linked;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignInActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+    }
+
+    /** Called when the user taps the SignIn button */
+    public void signIn(View view)
+    {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
