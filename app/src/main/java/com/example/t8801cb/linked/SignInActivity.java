b7c6b5d5466f2ffd2c5422d4911405c8a21869bc
martinbebey <bebeymartin@gmail.com>
2020-04-08 00:57:54 -0400

    added db class and signin logic

--- app/src/main/java/com/example/t8801cb/linked/SignInActivity.java
+++ app/src/main/java/com/example/t8801cb/linked/SignInActivity.java
@@ -3,22 +3,58 @@ package com.example.t8801cb.linked;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
 import android.os.Bundle;
 import android.view.View;
+import android.database.sqlite.*;
+import android.widget.TextView;
 
-public class SignInActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class SignInActivity extends AppCompatActivity
+{
+    private DatabaseHelper mDBHelper;
+    private SQLiteDatabase mDb;
+    private String username = "";
+    private String password = "";
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        mDBHelper = new DatabaseHelper(this);
+
+        try {
+            mDBHelper.updateDataBase();
+        } catch (IOException mIOException) {
+            throw new Error("UnableToUpdateDatabase");
+        }
+
+        try {
+            mDb = mDBHelper.getWritableDatabase();
+        } catch (SQLException mSQLException) {
+            throw mSQLException;
+        }
     }
 
     /** Called when the user taps the SignIn button */
     public void signIn(View view)
     {
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
+        textView = (TextView)findViewById(R.id.username_box);
+        username = textView.getText().toString();
+        textView = (TextView)findViewById(R.id.password_box);
+        password = textView.getText().toString();
+
+        Cursor cursor = mDb.rawQuery("SELECT username, password FROM users" + " WHERE username = '"+ username + "' AND password ='"+ password + "'", null);
+
+        if(cursor != null)
+        {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+        }
     }
 }
